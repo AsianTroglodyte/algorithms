e04f27c231328116b04d583bcb75c45c494142d3
Karan Swansi <kswansi2002@gmail.com>
2026-09-16 07:18:51 -0400

    added rotations and crucial notes for each.

--- binary_tree.py
+++ binary_tree.py
@@ -1,14 +1,10 @@
-import types
+import math
 
 
 class Node:
     """Binary Search Tree Node."""
 
-    def __init__(self,
-                 key,
-                 left_node=None,
-                 right_node=None,
-                 parent_node=None):
+    def __init__(self, key):
         """Initialize the values of the node."""
         self.left: Node = None
         self.right: Node = None
@@ -16,18 +12,12 @@ class Node:
         self.key: int = key
 
 
-node1 = Node(1)
-node2 = Node(2)
-node3 = Node(3)
+class Tree:
+    """Tree that merely contains Root node."""
 
-node4 = Node(4)
-node5 = Node(5)
-
-node4.left = node2
-node2.left = node1
-node2.right = node3
-
-node4.right = node5
+    def __init__(self, root: Node = None):
+        """Initialize the root node."""
+        self.root = root
 
 
 def inorder_tree_walk(node: Node) -> None:
@@ -36,6 +26,7 @@ def inorder_tree_walk(node: Node) -> None:
         inorder_tree_walk(node.left)
         print(node.key)
         inorder_tree_walk(node.right)
+
 
 def preorder_tree_walk(node: Node) -> None:
     """Do an preorderwalk of the binary tree."""
@@ -53,7 +44,7 @@ def postorder_tree_walk(node: Node) -> None:
         print(node.key)
 
 
-def tree_search(node: Node, key: int) -> None:
+def tree_search(node: Node, key: int) -> Node:
     """Search a binary search tree and return the node if it is found."""
     if node is None or key == node.key:
         return node
@@ -63,7 +54,7 @@ def tree_search(node: Node, key: int) -> None:
         return tree_search(node.right, key)
 
 
-def tree_minimum(node: Node) -> None:
+def tree_minimum(node: Node) -> Node:
     """Search a binary search tree to find the minimum value."""
     while node.left is not None:
         node = node.left
@@ -77,7 +68,21 @@ def tree_maximum(node: Node) -> Node:
     return node
 
 
-def tree_successor(node: Node):
+def tree_minimum_recursive(node: Node) -> Node:
+    """Search a binary search tree to find the minimum value using recursion."""
+    if (node.left is not None):
+        return tree_minimum(node.left)
+    return node
+
+
+def tree_maximum_recursive(node: Node) -> Node:
+    """Search a binary search tree to find the maximum value using recursion."""
+    if (node.right is not None):
+        return tree_minimum(node.right)
+    return node
+
+
+def tree_successor(node: Node) -> Node:
     """Find the next node that is visited in inorder traversal."""
     if node.right is not None:
         """If there is right node, then its smallest val is successor """
@@ -92,12 +97,137 @@ def tree_successor(node: Node):
         return parent_node
 
 
-print("inorder_tree_walk: ")
-inorder_tree_walk(node4)
-print("preorder_tree_walk: ")
-preorder_tree_walk(node4)
-print("postorder_tree_walk: ")
-postorder_tree_walk(node4)
+def transplant(tree: Tree, old_root: Node, replacement: Node):
+    """Transplant some subtree with a some other subtree."""
+    if old_root.parent is None:
+        tree.root = replacement
+    elif old_root == old_root.parent.left:
+        old_root.parent.left = replacement
+    else:
+        old_root.parent.right = replacement
+    if replacement is not None:
+        replacement.parent = old_root.parent
+
+
+def tree_delete(tree: Tree, node_to_delete: Node):
+    """Delete a node from a tree."""
+    if node_to_delete.left is None:
+        # replacing node with its right child
+        # Note that right child could be None as well which is valid.
+        transplant(tree, node_to_delete, node_to_delete.right)
+    elif node_to_delete.right is None:
+        # replacing node with its left child
+        transplant(tree, node_to_delete, node_to_delete.left)
+    else:
+        successor = tree_minimum(node_to_delete.right)
+        if successor != node_to_delete.right:  # is succesor farther down tree?
+            # replace successor by it's right child
+            transplant(tree, successor, successor.right)
+            successor.right = node_to_delete.right
+            successor.right.parent = successor
+        transplant(tree, node_to_delete, successor)  # replace node by its successor
+        successor.left = node_to_delete.left  # node's left child to successor
+        successor.left.parent = successor
+
+
+def tree_insert(tree: Tree, new_node: Node):
+    """Insert a node into a Tree."""
+    current_node = tree.root
+    parent_of_new_node = None
+
+    # search through tree, finding
+    while current_node is not None:
+        parent_of_new_node = current_node
+        if new_node.key < current_node.key:
+            current_node = current_node.left
+        else:
+            current_node = current_node.right
+
+    new_node.parent = parent_of_new_node
+    if parent_of_new_node is None:
+        tree.root = new_node
+    elif new_node.key < parent_of_new_node.key:
+        parent_of_new_node.left = new_node
+    else:
+        parent_of_new_node.right = new_node
+
+
+def is_valid_bst(node, lower_bound=-math.inf, upper_bound=math.inf):
+    """Check if the BST is a valid BST."""
+    if node is None:
+        return True
+
+    if not lower_bound < node.key < upper_bound:
+        return False
+
+    return (
+        is_valid_bst(node.left, lower_bound, node.key)
+        and
+        is_valid_bst(node.right, node.key, upper_bound)
+    )
+
+
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+
+node4 = Node(4)
+node5 = Node(5)
+
+# node 4 is the root node
+tree = Tree(node4)
+tree_insert(tree, node2)
+tree_insert(tree, node1)
+tree_insert(tree, node3)
+tree_insert(tree, node5)
+
+
+is_valid_bst(node4)
+
+
+node100 = Node(100)
+tree_insert(tree, node100)
+node69 = Node(69)
+tree_insert(tree, node69)
+node124 = Node(124)
+tree_insert(tree, node124)
+# print(is_valid_bst(node4))
+
+
+# def get_tree_node_list(node: Node, list: [Node]):
+#     """Get list of all tree nodes."""
+#     all_nodes = []
+#     if (node is not None):
+#         left_list = get_tree_node_list(node.left)
+#         if left_list is not None:
+#             all_nodes
+#         right_list = get_tree_node_list(node.right)
+
+# delete testing
+all_nodes = [node1, node2, node3, node4, node5, node100, node69, node124]
+
+for node in all_nodes:
+    # inorder_tree_walk(tree.root)
+    print(f"Deleting {node.key}")
+    inorder_tree_walk(tree.root)
+    tree_delete(tree, node)
+    print(is_valid_bst(node4))
+    print("\n")
+
+# found_minimum = tree_minimum_recursive(node4)
+# if found_minimum is not None:
+#     print(f"min_node_ref: {found_minimum}\n min_node_key: {found_minimum.key}")
+
+# found_maximum = tree_maximum_recursive(node4)
+# if found_maximum is not None:
+#     print(f"max_node_ref: {found_maximum}\n max_node_key: {found_maximum.key}")
+
+# print("inorder_tree_walk: ")
+# inorder_tree_walk(node4)
+# print("preorder_tree_walk: ")
+# preorder_tree_walk(node4)
+# print("postorder_tree_walk: ")
+# postorder_tree_walk(node4)
 # found_object = tree_search(node4, 2)
 # if found_object is not None:
 #     print(f"obj_ref: {found_object}\n obj_key: {found_object.key}")
--- red_black_trees.py
+++ red_black_trees.py
@@ -1,17 +1,92 @@
 import types
+from enum import Enum
 
 
-class node:
+class Color(Enum):
+    """Enum for node colors."""
+
+    RED = "red"
+    BLACK = "black"
+
+
+class Node:
     """Red black tree Node."""
 
     def __init__(self, key):
         """Initialize the values of the node."""
+        self.color: Color = None
         self.key = key
-        self.red = True
-        self.black = None
+        self.left = None
+        self.right = None
+        self.parent = None
+
+# 1. Every node is either red or black.
+# 2. The root is black.
+# 3. Every leaf (NIL) is black.
+# 4. If a node is red, then both its children are black.
+# 5. For each node, all simple paths from the node to descendant
+# leaves contain the same number of black nodes.
 
 
-node1 = node(1)
-node2 = node(2)
+class Tree:
+    """Tree that merely contains Root node."""
 
-print("hello world")
+    def __init__(self, root: Node = None):
+        """Initialize the root node."""
+        self.root = root
+
+
+def left_rotate(tree: Tree, rotate_down: Node):
+    """Rotate rotate_down left, promoting its right child, rotate_up."""
+    rotate_up = rotate_down.right
+
+    # Turn rotate_up's left subtree into rotate_down's right subtree.
+    rotate_down.right = rotate_up.left
+
+    # remaining conditions handle setting the parents attributes appropriately
+    if rotate_up.left is not None:  # If rotate_up's left subtree is not empty...
+        # ...then rotate_down becomes the parent of the subtree's root.
+        rotate_up.left.parent = rotate_down
+
+    # rotate_down's parent becomes rotate_up's parent.
+    rotate_up.parent = rotate_down.parent
+    if rotate_down.parent is None:  # If rotate_down was the root...
+        tree.root = rotate_up  # ...then rotate_up becomes the root.
+    elif rotate_down == rotate_down.parent.left:
+        # Otherwise, if rotate_down was a left child...
+        rotate_down.parent.left = rotate_up  # ...rotate_up becomes a left child.
+    else:
+        # Otherwise, rotate_down was a right child, and now rotate_up is.
+        rotate_down.parent.right = rotate_up
+
+    # Make rotate_down become rotate_up's left child.
+    rotate_up.left = rotate_down
+    rotate_down.parent = rotate_up
+
+
+def right_rotate(tree: Tree, rotate_down: Node):
+    """Rotate rotate_down right, promoting its left child, rotate_up."""
+    rotate_up = rotate_down.left
+
+    # Turn rotate_up's right subtree into rotate_down's left subtree.
+    rotate_down.left = rotate_up.right
+
+    # remaining conditions handle setting the parents attributes appropriately
+    if rotate_up.right is not None:  # If rotate_up's right subtree is not empty...
+        # ...then rotate_down becomes the parent of the subtree's root.
+        rotate_up.right.parent = rotate_down
+
+    # rotate_down's parent becomes rotate_up's parent.
+    rotate_up.parent = rotate_down.parent
+    if rotate_down.parent is None:  # If rotate_down was the root...
+        tree.root = rotate_up  # ...then rotate_up becomes the root.
+    elif rotate_down == rotate_down.parent.right:
+        # Otherwise, if rotate_down was a right child...
+        rotate_down.parent.right = rotate_up  # ...rotate_up becomes a right child.
+    else:
+        # Otherwise, rotate_down was a left child, and now rotate_up is.
+        rotate_down.parent.left = rotate_up
+
+    # Make rotate_down become rotate_up's right child.
+    rotate_up.right = rotate_down
+    rotate_down.parent = rotate_up
